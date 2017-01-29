87f7a801df77fa4743dc40d82f1a0b63e691f823
George Mponos <gmponos@gmail.com>
2017-01-29 12:36:41 +0200

    Removed the  and made it LanguageConstructSpacing in order to add more Language Constructs to have one space

---
+++ Webthink/Sniffs/WhiteSpace/LanguageConstructSpacingSniff.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Overrides the tokens of Squiz_Sniffs_WhiteSpace_LanguageConstructSpacingSniff
+ * in order to listen to more tokens.
+ *
+ * @see    Squiz_Sniffs_WhiteSpace_LanguageConstructSpacingSniff
+ * @author George Mponos <gmponos@gmail.com>
+ */
+class Webthink_Sniffs_WhiteSpace_LanguageConstructSpacingSniff extends Squiz_Sniffs_WhiteSpace_LanguageConstructSpacingSniff
+{
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return array_merge(
+            parent::register(),
+            [
+                T_YIELD,
+                T_THROW,
+                T_USE,
+                T_NAMESPACE,
+            ]
+        );
+    }
+}
