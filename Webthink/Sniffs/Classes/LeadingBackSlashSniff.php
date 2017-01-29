f3177f2345ee70f169ddbffb338e51a1eab7ce21
George Mponos <gmponos@gmail.com>
2017-01-29 18:31:26 +0200

    Disallow a backslash at FQN

---
+++ Webthink/Sniffs/Classes/LeadingBackSlashSniff.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Use statements to import classes must not begin with "\".
+ *
+ * Copied from `drupal/coder`
+ *
+ * @see https://github.com/klausi/coder
+ */
+class Webthink_Sniffs_Classes_LeadingBackSlashSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        return [T_USE];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        // Only check use statements in the global scope.
+        if (!empty($tokens[$stackPtr]['conditions'])) {
+            return;
+        }
+
+        $startPtr = $phpcsFile->findNext(
+            PHP_CodeSniffer_Tokens::$emptyTokens,
+            ($stackPtr + 1),
+            null,
+            true
+        );
+
+        if ($startPtr !== null && $tokens[$startPtr]['code'] === T_NS_SEPARATOR) {
+            $fix = $phpcsFile->addFixableError(
+                'When importing a class with "use", do not include a leading \\',
+                $startPtr,
+                'SeparatorStart'
+            );
+
+            if ($fix === true) {
+                $phpcsFile->fixer->replaceToken($startPtr, '');
+            }
+        }
+    }
+}
