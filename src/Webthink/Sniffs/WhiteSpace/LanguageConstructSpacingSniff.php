0ccdf77202f6347015d276a5f1bb641c13b95d86
George Mponos <gmponos@gmail.com>
2017-08-02 20:16:49 +0300

    Docblock changes

--- src/Webthink/Sniffs/WhiteSpace/LanguageConstructSpacingSniff.php
+++ src/Webthink/Sniffs/WhiteSpace/LanguageConstructSpacingSniff.php
@@ -7,17 +7,17 @@ use PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\LanguageConstructSpacingSn
 /**
  * Overrides the tokens of Squiz_Sniffs_WhiteSpace_LanguageConstructSpacingSniff
  * in order to listen to more tokens.
- * 
+ *
  * At the time that this is written there is a merge request open at official codesniffer
  * that fixes this.
  * 
+ * https://github.com/squizlabs/PHP_CodeSniffer/pull/1337
  * 
- *
  * @see Squiz_Sniffs_WhiteSpace_LanguageConstructSpacingSniff
  * @author George Mponos <gmponos@gmail.com>
  * @license MIT
  */
-class LanguageConstructSpacingSniff extends SquizLanguageConstructSpacingSniff
+final class LanguageConstructSpacingSniff extends SquizLanguageConstructSpacingSniff
 {
     /**
      * @inheritdoc
